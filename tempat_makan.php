ba70081ce94b9207ab50407aa924da221fcfbeb9
SELLY PRATIWI <sellypratiwi300406@gmail.com>
2024-11-27 23:30:45 +0700

    Update tempat_makan.php

--- tempat_makan.php
+++ tempat_makan.php
@@ -8,13 +8,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tempat makan</title>
-    <link rel="stylesheet" href="style_tempat_makan.css"
+    <title>Tempat_makan</title>
+    <link rel="stylesheet" href="css/style_tempat_makan.css">
        
-    
     <!-- Header -->
     <div class="header">
-        <a href="dasboard.php" class="header">MLAKURASO</a>
+        <div class="header">────୨Mlakurasoৎ────</div>
         </div>
     </div>
 
@@ -28,7 +27,7 @@
     <div class="menu-grid">
         <div class="menu-item">
             <div class="discount">20% off</div>
-            <img src="ayamgoreng.jpg" alt="Menu Item">
+            <img src="img/ayamgoreng.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Ayam Goreng Crispy</div>
                 <div class="rating">⭐ 4.7 • 30 min</div>
@@ -37,7 +36,7 @@
         </div>
         <div class="menu-item">
             <div class="discount">50% off</div>
-            <img src="burger.jpg" alt="Menu Item">
+            <img src="img/burger.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Burger Good Day</div>
                 <div class="rating">⭐ 4.5 • 25 min</div>
@@ -45,7 +44,7 @@
             </div>
         </div>
         <div class="menu-item">
-            <img src="piza.jpg" alt="Menu Item">
+            <img src="img/piza.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Pizza Hut</div>
                 <div class="rating">⭐ 4.6 • 40 min</div>
@@ -54,7 +53,7 @@
         </div>
         <div class="menu-item">
             <div class="discount">40% off</div>
-            <img src="richeese.jpg" alt="Menu Item">
+            <img src="img/richeese.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Richeese Factory</div>
                 <div class="rating">⭐ 4.8 • 30 min</div>
@@ -63,7 +62,7 @@
         </div>
         <div class="menu-item">
             <div class="discount">15% off</div>
-            <img src="seblak.jpg" alt="Menu Item">
+            <img src="img/seblak.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Seblak Teh Seny</div>
                 <div class="rating">⭐ 4.8 • 10 min</div>
@@ -71,7 +70,7 @@
             </div>
         </div>
         <div class="menu-item">
-            <img src="martabak.jpg" alt="Menu Item">
+            <img src="img/martabak.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Martabak Pelanggi</div>
                 <div class="rating">⭐ 4.6 • 40 min</div>
@@ -79,7 +78,7 @@
             </div>
         </div>
         <div class="menu-item">
-            <img src="mieaceh.jpg" alt="Menu Item">
+            <img src="img/mieaceh.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Mie Aceh Lhokseumawe</div>
                 <div class="rating">⭐ 4.7 • 40 min</div>
@@ -87,7 +86,7 @@
             </div>
         </div>
         <div class="menu-item">
-            <img src="baksosolo.jpg" alt="Menu Item">
+            <img src="img/baksosolo.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Bakso Solo</div>
                 <div class="rating">⭐ 4.6 • 40 min</div>
@@ -96,7 +95,7 @@
         </div>
         <div class="menu-item">
             <div class="discount">15% off</div>
-            <img src="sotopadang.jpg" alt="Menu Item">
+            <img src="img/sotopadang.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Soto Padang Putri Chaniago</div>
                 <div class="rating">⭐ 4.5 • 15 min</div>
@@ -104,7 +103,7 @@
             </div>
         </div>
             <div class="menu-item">
-            <img src="baksobakar.jpg" alt="Menu Item">
+            <img src="img/baksobakar.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Bakso Bakar Uni Bakwan</div>
                 <div class="rating">⭐ 4.8 • 10 min</div>
@@ -112,7 +111,7 @@
             </div>
         </div>
         <div class="menu-item">
-            <img src="jasuke.jpg" alt="Menu Item">
+            <img src="img/jasuke.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Kedai Jasuke</div>
                 <div class="rating">⭐ 4.7 • 30 min</div>
@@ -121,7 +120,7 @@
         </div>
         <div class="menu-item">
             <div class="discount">10% off</div>
-            <img src="saladbuah.jpg" alt="Menu Item">
+            <img src="img/saladbuah.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Kiyowo Salad Buah</div>
                 <div class="rating">⭐ 4.8 • 10 min</div>
@@ -129,7 +128,7 @@
             </div>
         </div>
         <div class="menu-item">
-            <img src="sate.jpg" alt="Menu Item">
+            <img src="img/sate.jpg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Sate Madura Cak Wil</div>
                 <div class="rating">⭐ 4.9 • 10 min</div>
@@ -137,7 +136,7 @@
             </div>
         </div>
         <div class="menu-item">
-            <img src="jus.jpeg" alt="Menu Item">
+            <img src="img/jus.jpeg" alt="Menu Item">
             <div class="menu-item-content">
                 <div class="name">Lapo Juice</div>
                 <div class="rating">⭐ 4.8 • 5 min</div>
