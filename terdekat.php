cad3be53edd180b879d19cc0c7c2384f2e43d34f
DIDAN <blueocean.didan@gmail.com>
2024-11-29 14:01:48 +0700

    Update terdekat.php

--- terdekat.php
+++ terdekat.php
@@ -3,7 +3,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="style_terdekat.css">
+<link rel="stylesheet" href="css/style_terdekat.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
